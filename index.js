34014c75555b320e4b91795f0562e592a6bf5df5
muzhiwa <muzhdawafa2008@gmail.com>
2024-03-11 17:09:02 +0430

    worked and updated css, html, and javascript

--- index.js
+++ index.js
@@ -1,14 +1,18 @@
-function search(event) {
+function searchCity(event) {
     event.preventDefault();
     let searchInput = document.querySelector(".enter-city");
     let h1 = document.querySelector("#city-name");
     h1.innerHTML = searchInput.value;
+    let key = "71260f74c15bct2a0o7b939feaa3813b";
+    let city = searchInput.value;
+    let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${key}&units=metric`;
+    axios.get(apiUrl).then(displayTemperature);
 }
+
 let form = document.querySelector("#search-form");
-  form.addEventListener("submit", search);
+  form.addEventListener("submit", searchCity);
   
-  let now = new Date();
-  let time = document.querySelector("#date");
+function formatDate(date) {
   let days = [
     "Sunday",
     "Monday",
@@ -18,13 +22,35 @@ let form = document.querySelector("#search-form");
     "Friday",
     "Saturday",
   ];
-  let day = days[now.getDay()];
-  let hours = now.getHours();
-  let minutes = now.getMinutes();
+  let day = days[date.getDay()];
+  let hours = date.getHours();
+  let minutes = date.getMinutes();
   if (minutes < 10) {
     minutes = `0${minutes}`;
   }
   if (hours < 10) {
-    minutes = `0${hours}`;
+    hours = `0${hours}`;
   }
-  time.innerHTML = `${day} ${hours}:${minutes},`;
+  return `${day} ${hours}:${minutes},`;
+}
+function displayTemperature(response) {
+    console.log(response.data); 
+    let temperature = Math.round(response.data.temperature.current);
+    let temperatureElement = document.querySelector("#temperature");
+    temperatureElement.innerHTML = `${temperature}`;
+    let descriptionElement = document.querySelector("#description");
+    descriptionElement.innerHTML = response.data.condition.description;
+    let humidityElement = document.querySelector("#humidity");
+    humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
+    let windSpeedElement = document.querySelector("#wind-speed");
+    windSpeedElement.innerHTML = `${response.data.wind.speed}km/h`;
+    let timeElement = document.querySelector("#date");
+    let date = new Date(response.data.time * 1000);
+    timeElement.innerHTML = formatDate(date);
+    let iconElement = document.querySelector("#icon");
+    iconElement.innerHTML = `<img src="${response.data.condition.icon_url}" class="weather-app-icon" />`;
+    let cityElement = document.querySelector("#city-name");
+    cityElement.innerHTML = response.data.city;
+}
+  document.querySelector(".enter-city").value = "Kabul";
+searchCity(new Event("submit"));
